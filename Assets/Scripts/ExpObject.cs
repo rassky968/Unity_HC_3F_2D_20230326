c360fb450acdd09ce2baea4f9056e2cad598a99f
rassky968 <135485266+rassky968@users.noreply.github.com>
2023-06-04 16:06:36 +0800

    2023/6/4Update

---
+++ Assets/Scripts/ExpObject.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class ExpObject : MonoBehaviour
+{
+    [Header("可以吃這個物件的名稱")]
+    public string nameTarget = "犀牛";
+    [Header("移動速度"), Range(0, 500)]
+    public float moveSpeed = 3.5f;
+    [Header("吃掉物件距離"), Range(0, 5)]
+    public float eatDistance = 1.5f;
+    [Header("經驗值"), Range(0, 5000)]
+    public float exp = 30;
+
+    private Transform player;
+    private bool playerInArea;
+
+    private LevelManager levelManager;
+
+    private void Awake()
+    {
+        player = GameObject.Find(nameTarget).transform;
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    private void Update()
+    {
+        if (playerInArea)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+
+            float d = Vector2.Distance(transform.position, player.position);
+            if(d < eatDistance)
+            {
+                levelManager.GetExp(exp);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //print($"<color=#ff6699>{collision.name}</color>");
+
+        if (collision.name.Contains(nameTarget))
+        {
+            playerInArea = true;
+        }
+    }
+}
